de53906fac8adb1d58d90f178e37a99aa29e28de
raphaellebas <raphael.lebas@outlook.fr>
2026-02-21 15:39:17 +0100

    fix: card and main

--- index.php
+++ index.php
@@ -21,28 +21,66 @@
                 <ul class="slider-container">
                     <li class="product-result">
                         <div class="column">
-                               <div class="card">
-                        <div class="img-card">
-                            <figure><img src="./assets/img/jpg/rogstrix.jpg" alt=""></figure>
-                        </div>
-                        <div class="container-card">
-                            <div class="container-information">
-                                <div class="svg-logo">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M7.5 7v-.5a4.5 4.5 0 0 1 9 0V7H19c.552 0 1 .449 1 1.007v12.001c0 1.1-.895 1.992-1.994 1.992H5.994A1.994 1.994 0 0 1 4 20.008v-12C4 7.45 4.445 7 5 7zM9 7h6v-.5a3 3 0 0 0-6 0zM7.5 7v4H9V7zM15 7v4h1.5V7z"/></svg>
+                            <div class="card">
+                                <div class="img-card">
+                                    <figure><img src="./assets/img/jpg/rogstrix.jpg" alt=""></figure>
                                 </div>
-                                <div class="information">
-                                    <h1 class="text-h1-media">Product name</h1>
-                                    <p class="text-h2-media">Ob haec et huius modi multa, quae cernebantur in paucis, omnibus timeri sunt coepta. et ne tot malis dissimulatis paulatimque serpentibus acervi crescerent aerumnarum.</p>
+                                <div class="container-card">
+                                    <div class="container-information">
+                                        <div class="svg-container">
+                                            <div class="svg-logo">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24">
+                                                    <path fill-rule="evenodd"
+                                                        d="M7.5 7v-.5a4.5 4.5 0 0 1 9 0V7H19c.552 0 1 .449 1 1.007v12.001c0 1.1-.895 1.992-1.994 1.992H5.994A1.994 1.994 0 0 1 4 20.008v-12C4 7.45 4.445 7 5 7zM9 7h6v-.5a3 3 0 0 0-6 0zM7.5 7v4H9V7zM15 7v4h1.5V7z" />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                        <div class="information">
+                                            <h1 class="text-h1-media">Product name</h1>
+                                            <p class="text-h2-media">Ob haec et huius modi multa, quae cernebantur in
+                                                paucis, omnibus timeri sunt coepta. et ne tot malis dissimulatis
+                                                paulatimque serpentibus acervi crescerent aerumnarum.</p>
+                                        </div>
+
+                                    </div>
+                                    <div class="info">
+                                        <div class="rank">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                                                viewBox="0 0 24 24">
+                                                <path
+                                                    d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z" />
+                                            </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                                                viewBox="0 0 24 24">
+                                                <path
+                                                    d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z" />
+                                            </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                                                viewBox="0 0 24 24">
+                                                <path
+                                                    d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z" />
+                                            </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                                                viewBox="0 0 24 24">
+                                                <path
+                                                    d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z" />
+                                            </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                                                viewBox="0 0 24 24">
+                                                <path class="unrated"
+                                                    d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z" />
+                                            </svg>
+                                        </div>
+                                        <h1 class="price text-h1-media">1981€</h1>
+                                    </div>
+                                    <div class="detail">
+                                        <a href="./pages/product-detail.php">En savoir plus</a>
+                                    </div>
                                 </div>
                             </div>
+
                         </div>
-                    </div>
-                    
-                        </div>
-                 
-                    <!-- <a href="./pages/product-detail.php"> -->
-                     
-                        </a>
                     </li>
                     <li></li>
                     <li></li>
